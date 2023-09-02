4a691bf4ec2bfaf7917d4907bbec380462768100
Vivek Vaish <vaishvivek634@gmail.com>
2023-09-02 19:56:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [red, setRed] = useState(255);
+  const [green, setGreen] = useState(255);
+  const [blue, setBlue] = useState(255);
+  const headingColor = red <= 100 && green <= 100 && blue <= 100 ? 'white' : "black";
+  const color = `rgb(${red}, ${green}, ${blue})`;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' style={{ backgroundColor: color, color: headingColor }}>
+      <h2>Color Generator</h2>
+      <div className="label">rgb({red}, {green}, {blue})</div>
+
+      <label htmlFor="red">Red:</label>
+      <div>
+        <input type="number" id="red" value={red} onChange={e => setRed(e.target.value)} />
+        <input type="range" min='0' max='255' value={red} onChange={e => setRed(e.target.value)} />
+      </div>
+      <label htmlFor="green">Green:</label>
+      <div>
+        <input type="number" id="green" value={green} onChange={e => setGreen(e.target.value)} />
+        <input type="range" min='0' max='255' value={green} onChange={e => setGreen(e.target.value)} />
+      </div>
+      <label htmlFor="blue">Blue:</label>
+      <div>
+        <input type="number" id="blue" value={blue} onChange={e => setBlue(e.target.value)} />
+        <input type="range" min='0' max='255' value={blue} onChange={e => setBlue(e.target.value)} />
+      </div>
     </div>
   );
 }
